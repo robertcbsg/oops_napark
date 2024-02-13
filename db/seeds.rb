d7ffeb5e4202856e0917489db26222c45527567b
Robert Daniel F. Cabasag <robertcbsg@gmail.com>
2024-02-14 01:36:50 +0800

    create and run db seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,67 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-clocks = Clock.create([{ datetime_now: Time.now}])
+clock = Clock.create([{ datetime_now: Time.now}])
+
+parking_lot = ParkingLot.create([
+    {
+        name: "OOPs Parking Lot"
+    }
+])
+
+parking_entry_points = ParkingEntryPoint.create([
+    {
+        name: "North Exit",
+        parking_lot_id: parking_lot[0].id
+    },
+    {
+        name: "West Exit",
+        parking_lot_id: parking_lot[0].id
+    },
+    {
+        name: "Southwest Exit",
+        parking_lot_id: parking_lot[0].id
+    }
+])
+
+parking_slots = ParkingSlot.create([
+    {
+        size: 2,
+        is_available: false,
+        parking_lot_id: parking_lot[0].id
+    },
+    {
+        size: 1,
+        is_available: false,
+        parking_lot_id: parking_lot[0].id
+    },
+    {
+        size: 0,
+        is_available: false,
+        parking_lot_id: parking_lot[0].id
+    }
+])
+
+# [RDC] For demo purposes
+parking_slots.each do |slot|
+    parking_entry_points.each do |entry|
+        DistanceFromEntry.create({
+            distance: rand(1..5),
+            parking_slot_id: slot.id,
+            parking_entry_point_id: entry.id
+        })
+    end
+end
+
+vehicles = Vehicle.create([
+    {
+        plate_number: "CDN103",
+        is_parked: false,
+        size: 1
+    },
+    {
+        plate_number: "ASI129",
+        is_parked: false,
+        size: 0
+    }
+])
